42c29f402eb4c0d28abddde5b8205b0bbcfd2230
Enoch Fabiyi <ayomikun204@gmail.com>
2022-07-13 23:15:39 +0100

    chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
     <div>
