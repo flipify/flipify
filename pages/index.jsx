c759915edeac66e5d0e9b7e316b80f52a2dba04f
Enoch Fabiyi <ayomikun204@gmail.com>
2023-01-28 18:41:44 +0100

    removed unused function

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,40 +14,10 @@ export default function Home() {
 
     useEffect(() => {
         if (session && status === 'authenticated') {
-            window.close();
             router.push(`/panel`);
             console.log(session);
         }
     }, [session, status]);
-
-    const popupCenter = (url, title) => {
-        const dualScreenLeft = window.screenLeft ?? window.screenX;
-        const dualScreenTop = window.screenTop ?? window.screenY;
-        const width =
-            window.innerWidth ??
-            document.documentElement.clientWidth ??
-            screen.width;
-
-        const height =
-            window.innerHeight ??
-            document.documentElement.clientHeight ??
-            screen.height;
-
-        const systemZoom = width / window.screen.availWidth;
-
-        const left = (width - 500) / 2 / systemZoom + dualScreenLeft;
-        const top = (height - 550) / 2 / systemZoom + dualScreenTop;
-
-        const newWindow = window.open(
-            url,
-            title,
-            `width=${500 / systemZoom},height=${
-                550 / systemZoom
-            },top=${top},left=${left}`
-        );
-
-        newWindow?.focus();
-    };
 
     return (
         <div className="bgContainer">
